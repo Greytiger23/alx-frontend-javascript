c087f409d08a6ac0ec792c688cfe9253bb868765
Greytiger23 <isabelmothoa49@gmail.com>
2024-06-24 18:33:38 -0700

    0x00-ES6 basic

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,29 @@
+export default function createIteratorObject(report) {
+  const departments = Object.keys(report);
+  const index = 0;
+  const currentDepartment = 0;
+
+  return {
+    [Symbol.iterator]() {
+      return this;
+    },
+    next() {
+      if (index < departments.length) {
+        if (index === 0 || report[departments[currentDepartment]].length === 0) {
+          currentDepartment++;
+          index++;
+        }
+        if (currentDepartment < departments.length) {
+          return {
+            value: report[departments[currentDepartment - 1]][report[departments[currentDepartment - 1]].length - 1],
+            done: false
+          };
+        } else {
+          return { done: true };
+        }
+        } else {
+          return { done: true };
+        }
+      }
+  };
+}
