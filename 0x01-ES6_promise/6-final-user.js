420553d25335fbacbcec7f250d0cf8e42e0a1bf6
Greytiger23 <isabelmothoa49@gmail.com>
2024-06-26 15:46:13 -0700

    0x01- ES6 promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,24 +2,34 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const signUpPromise = signUpUser(firstName, lastName).then((value) => ({
-    status: 'fulfilled',
-    value: value,
-  }),
-  (error) => ({
-    status: 'rejected',
-    value: error,
-  }));
+  const signUpPromise = signUpUser(firstName, lastName).then(
+    (value) => ([
+      {
+        status: 'fulfilled',
+        value,
+      },
+    ]),
+    (error) => ([
+      {
+        status: 'rejected',
+        value: error,
+      },
+    ]),
+  );
 
   const uploadPromise = uploadPhoto(fileName).then(
-    (value) => ({
-      status: 'fulfilled',
-      value: value,
-    }),
-    (error) => ({
-      status: 'rejected',
-      value: error,
-    }),
+    (value) => ([
+      {
+        status: 'fulfilled',
+        value,
+      },
+    ]),
+    (error) => ([
+      {
+        status: 'rejected',
+        value: error,
+      },
+    ]),
   );
 
   return Promise.allSettled([signUpPromise, uploadPromise]);
