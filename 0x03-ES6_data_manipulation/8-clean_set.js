bfb86fd9beae421af2bfb0649435faa22bf475d3
Greytiger23 <isabelmothoa49@gmail.com>
2024-07-02 17:00:20 -0700

    0x03-ES6 data manipulation

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,12 @@
 function cleanSet(set, startString) {
   if (typeof startString !== 'string' || startString.length === 0) {
-    return '';
+    return ' ';
   }
 
   return [...set]
     .filter((value) => value.startsWith(startString))
     .map((value) => value.slice(startString.length))
-    .join('_');
+    .join('-');
 }
 
 export default cleanSet;
